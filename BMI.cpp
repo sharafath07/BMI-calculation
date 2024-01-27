dbdc8edf4bfd535460bd4dfcdf7cebedabba48b8
sharafath07 <142240872+sharafath07@users.noreply.github.com>
2024-01-27 08:22:24 +0000

    update

--- BMI.cpp
+++ BMI.cpp
@@ -5,7 +5,7 @@ int main()
 {
     float W, H, BMI, sq;
     int min, max, j, i;
-    
+
     cout << "Enter your weight in kg\n" ;
     cin >> W;
     cout << "Enter your height in metre\n" ;
@@ -13,36 +13,36 @@ int main()
 
     sq=H*H;
     BMI=W/sq;
+
     cout << "Body Mass Index =" << BMI << "\n";
 
-    for(i=0;i<10000;i++)
+    for (i = 0; i < 10000; i++)
     {
-    min=i/sq;
-    if(min==18)
-    break;
-    else
-    continue;
+        min = i / sq;
+        if (min == 18)
+            break;
+        else
+            continue;
     }
-    for(j=0;j<10000;j++)
+    for (j = 0; j < 10000; j++)
     {
-    max=j/sq;
-    if(max==25)
-    break;
-    else
-    continue;
+        max = j / sq;
+        if (max == 24)
+            break;
+        else
+            continue;
     }
-
-    if(BMI>24.9)
+    if (BMI>24.9)
     {
-    if(BMI>30)
-    cout << "It is at obuseweight range.\nYour healthy weight range is between " << i << "kg" << " and " << j << "kg" ;
-    else
-    cout << "It is at overweight range.\nYour healthy weight range is between " << i << "kg" << " and " << j << "kg" ;
+        if(BMI>30)
+            cout << "It is at obuseweight range.\nYour healthy weight range is between " << i << "kg" << " and " << j << "kg" ;
+        else
+            cout << "It is at overweight range.\nYour healthy weight range is between " << i << "kg" << " and " << j << "kg" ;
     }
     else if(BMI<18.5)
-    cout << "It is at underweight range.\nYour healthy weight range is between " << i << "kg" << " and " << j << "kg" ;
+        cout << "It is at underweight range.\nYour healthy weight range is between " << i << "kg" << " and " << j << "kg" ;
     else
-    cout << "It is at healthy weight range.\nYou want to maintain your weight in between " << i << "kg" << " and " << j << "kg" ;
+        cout << "It is at healthy weight range.\nYou want to maintain your weight in between " << i << "kg" << " and " << j << "kg" ;
 
-    return 0;
+    return 5;
 }
